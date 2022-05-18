f6f05cba641d1cb3105a05cb3f20a17747a78587
Astating <cguilb3@gmail.com>
2022-05-18 16:11:28 +0200

    adding routes (tuto)

--- tuto.go
+++ tuto.go
@@ -26,24 +26,75 @@ var events = allEvents{
 
 }
 
+func homeLink(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome Home !")
+}
+
 func createEvent(w http.ResponseWriter, r *http.Request){
 	var newEvent event
-	reqBody, err:= ioutil.ReadAll(r.Body)
-	if err != nil{
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
 		fmt.Fprintf(w, "ça marche pas")
 	}
 	json.Unmarshal(reqBody, &newEvent)
 	events = append(events, newEvent)
 	w.WriteHeader(http.StatusCreated)
+
 	json.NewEncoder(w).Encode(newEvent)
 }
 
-func homeLink(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Welcome Home !")
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+		}
+	}
+}
+
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
+
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+	var updatedEvent event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			events = append(append(events[:i], singleEvent), events[i+1:]...)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
 }
 
 func main(){
 	router:= mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
